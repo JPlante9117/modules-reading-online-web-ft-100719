d60ff6cf6c9d13216fd8317f632e2b99fe9fd616
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-16 18:21:19 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,5 +2,13 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  extend MetaDancing
+  include FancyDance::InstanceMethods
+  include Dance
+  extend FancyDance::ClassMethods
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
 end
